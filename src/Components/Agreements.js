ed8ff7b442af9bf808fae37444db7e79b39207c0
Fathir Ahmed Shishir <mfa.shishir@gmail.com>
2022-11-19 18:10:23 +0100

    fonts,Header added

---
+++ src/Components/Agreements.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Agreements = () => {
+    return (
+        <div>
+            <h1>Hi! I am from Agreements </h1>
+        </div>
+    );
+};
+
+export default Agreements;
